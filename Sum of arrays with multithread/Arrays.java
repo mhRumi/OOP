4f84c0ab2fdf28f23ff1b327ae9d7536699c07ed
M.H Rumi <43205294+mhRumi@users.noreply.github.com>
2019-03-27 03:19:19 +0600

    Update and rename empty to Arrays.java

---
+++ Sum of arrays with multithread/Arrays.java
@@ -0,0 +1,14 @@
+
+package multithread;
+
+public class Arrays {
+    
+    public static int[] A = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    public static int[] B = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    public static int[] C = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    public static int[] D = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    public static int[] E = new int[10];
+    public static int[] F = new int[10];
+    public static int[] G = new int[10];
+    
+}
