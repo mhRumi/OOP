7d8d5afb99aa53409481be6adf9dfc2963e4b480
M.H Rumi <43205294+mhRumi@users.noreply.github.com>
2019-03-30 11:53:11 +0600

    Create Throws.java

---
+++ Assignment_2/Throws.java
@@ -0,0 +1,16 @@
+
+package Exceptions;
+
+
+public class Throws {
+    
+    int a,b;
+
+    public int division(int a,int b) throws ArithmeticException
+    {
+        int t ;
+        t = a/b;
+        return t;
+    }
+    
+}
